cf7270fbd1b78bdc230c6f3784da5354c545369c
sudio <suip.shesta@outlook.com>
2018-12-25 16:27:40 +0545

    customized builder

---
+++ src/js/control/targetform.js
@@ -0,0 +1,36 @@
+import control from '../control'
+
+/**
+ * Text input class
+ * Output a <input type="text" ... /> form element
+ */
+export default class targetForm extends control {
+
+  /**
+   * build a text DOM element, supporting other jquery text form-control's
+   * @return {Object} DOM Element to be injected into the form.
+   */
+  build() {
+    const tabRand= Math.random().toString(36).substring(2, 15)
+    const addDom = '<div id="subTabs_'+tabRand+'"> <ul> <li><a href="#'+tabRand+'"><input type="text" class="tab_input" placeholder="tabname"/></a></li> </ul> <div id="'+tabRand+'"><li><input type="text" class="sub_tab_input" placeholder="tabname"/> <i class="subliRev fa fa-times" aria-hidden="true" data-tabid="1"></i></li></div></div><button id="addParTab">Add Main list</button><button id="addList">Add Sub List</button>'
+    $('.asdf-wrap').find('.input-wrap').removeClass('input-wrap').addClass('newInputTab').html(addDom)
+    $('#subTabs_'+tabRand)
+      .tabs()
+      .addClass('ui-tabs-vertical ui-helper-clearfix')
+      // console.log(this.dom)
+    return this.dom
+  }
+ 
+  /**
+   * onRender callback
+   */
+  onRender() {
+    // Set userData if available
+    if (this.config.userData) {
+      $(this.dom).val(this.config.userData[0])
+    }
+  }
+}
+
+// register this control for the following types & text subtypes
+control.register(['targetForm'], targetForm)
